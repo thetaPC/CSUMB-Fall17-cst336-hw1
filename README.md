14b93fc9b1efeb72156f198a24afbb11dddc4524
Maria Loza <mloza@csumb.edu>
2017-09-07 04:59:41 +0000

    first init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Homework 1
+
+## Maria Loza
+
+## CST 336
---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Homework 1</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a id="main" class="navbar-brand " href="index.php">Homework 1</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="html/page1.html">Page 1</a></li>
+                <li><a href="html/page2.html">Page 2</a></li>
+                <li><a href="html/page3.html">Page 3</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <!--<center>-->
+                    <img id="mainImg" alt="ai" src="http://analyticsindiamag.com/wp-content/uploads/2017/02/Banner-2.jpg" class="img-rounded" >
+                    <p class="space"><a href="http://www.webopedia.com/TERM/A/artificial_intelligence.html" target="_blank">Artificial intelligence</a> involves making computers be more like humans.</p>
+                <!--</center>-->
+            </div>
+        </div>
+    </div>
+    
+    <footer class="footer">
+      <div class="container">
+        <div class="text-muted">
+            <hr class="gray">
+            CST 336. &copy; 2017 Loza <br>
+        </div>
+      </div>
+    </footer>
+
+</body>
+
+</html>
